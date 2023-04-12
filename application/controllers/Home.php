3dcb2bb876ffc08abe107c94df254464a54c5bbd
Mark Kenneth Ferrer <hirookenji02@gmail.com>
2023-04-12 17:45:28 +0800

    First

---
+++ application/controllers/Home.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+        $this->load->helper('url');
+        $data['title'] = "Home";
+		$this->load->view('include/header_view', $data);
+        $this->load->view('include/home_view');
+        $this->load->view('include/footer_view');
+        
+	}
+    public function view_products(){
+        $this->load->helper('url');
+        $this->load->model('Products_model');
+        $data['products'] = $this->Products_model->getAllProducts();
+        $data['title'] = "View All Products";
+        $this->load->view('include/header_view', $data);
+        $this->load->view('products_view', $data);
+        $this->load->view('include/footer_view');
+
+    }
+}
---
+++ application/views/include/home_view.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col-4"></div>  
+        <div class="col mt-5">
+            <h2>Welcome to our karindirya</h2>
+            <a href="<?= base_url('Home/view_products') ?>" class="btn btn-info">View Items</a>  
+    </div>
+</div>
